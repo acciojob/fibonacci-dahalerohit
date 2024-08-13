74af53b0abb61c6fb558fa18adcdbebbf2ae3d32
Rohit Dahale <151941395+dahalerohit@users.noreply.github.com>
2024-08-13 21:48:22 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,5 +1,17 @@
 function fibonacci(num) {
 // your code here
+	 if (num === 1) return 0;
+    if (num === 2) return 1;
+
+    let a = 0, b = 1, fib = 1;
+
+    for (let i = 3; i <= num; i++) {
+        fib = a + b;
+        a = b;
+        b = fib;
+    }
+
+    return fib;
 }
 
 module.exports = fibonacci;
